5ac39cfcc75f921acd792dada46151207e0277c3
gomathi0917 <gomathikallimuthu1221@gmail.com>
2026-07-31 10:40:21 +0530

    Time: 1 ms (76.39%), Space: 42.6 MB (59.73%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
+//The addition od ODD and EVEN numbers and evaluating the GCD will give the number of elements.
